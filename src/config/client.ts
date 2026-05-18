6dd24b1e894b245bb0d4cec7d3f42447e3ce73f6
Gabriel S <gabrielsousa7418@gmail.com>
2026-05-17 22:59:38 -0300

    fix: remove prisma pg adapter

--- src/config/client.ts
+++ src/config/client.ts
@@ -1,6 +1,5 @@
-import * as dotenv from "dotenv";
-import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
+import * as dotenv from "dotenv";
 
 dotenv.config();
 
@@ -8,15 +7,7 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient;
 };
 
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL + "?sslmode=require",
-});
-
-export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    adapter,
-  });
+export const prisma = globalForPrisma.prisma ?? new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
